4b00e435ba0a3c86f5f70290733a8de37c23bd3f
Derell Licht <derelict@comcast.net>
2025-07-28 15:56:50 -0700

    code cleanup

--- about.cpp
+++ about.cpp
@@ -48,6 +48,9 @@ HISTORY:    Date:      Author:     Comment:
 //static BOOL CALLBACK AboutDlgProc(HWND hdlg, UINT uMessage, WPARAM wparam, LPARAM lparam)
 static INT_PTR CALLBACK AboutDlgProc(HWND hdlg, UINT uMessage, WPARAM wparam, LPARAM lparam)
 {
+#define  BUF_LEN  255
+   char buf[BUF_LEN+1];
+   
    switch(uMessage) {
    case WM_INITDIALOG:
       SetWindowText(GetDlgItem(hdlg, IDC_VERNUM), VerNum) ;
@@ -58,11 +61,9 @@ static INT_PTR CALLBACK AboutDlgProc(HWND hdlg, UINT uMessage, WPARAM wparam, LP
    case WM_COMMAND:
       switch (LOWORD(wparam)) {
       case IDC_WEBLINK:
-         ShellExecute(hdlg, "open", "http://derelllicht.42web.io/wbigcalc.html", "", "", SW_SHOW);
-         return TRUE;
-         
       case IDC_WEBLINK2:
-         ShellExecute(hdlg, "open", "https://github.com/DerellLicht/WBigCalc", "", "", SW_SHOW);
+         GetDlgItemText(hdlg, LOWORD(wparam), buf, BUF_LEN);
+         ShellExecute(hdlg, "open", buf, "", "", SW_SHOW);
          return TRUE;
          
       case IDOK:
